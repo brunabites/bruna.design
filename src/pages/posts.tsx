2b464efe6032e1f0fb4d1e40b961c206365e38e7
Bruna Bites <bruna.bites@gmail.com>
2022-11-01 10:11:14 -0300

    Update posts.tsx

--- src/pages/posts.tsx
+++ src/pages/posts.tsx
@@ -66,6 +66,29 @@ const Posts = ({ posts }: InferGetStaticPropsType<typeof getStaticProps>) => {
                 marketing, and all things digital.
               </p>
             </div>
+            <div className="relative w-full mb-4">
+              <input
+                aria-label="Search all posts"
+                type="text"
+                onChange={(e) => setSearchValue(e.target.value)}
+                placeholder="Search all posts"
+                className="hidden w-full px-4 py-2 text-gray-900 bg-white rounded-md focus:border-warning-500 focus:ring-warning-500"
+              />
+              <svg
+                className="hidden absolute w-5 h-5 text-gray-500 right-3 top-3"
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth={2}
+                  d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"
+                />
+              </svg>
+            </div>
           </div>
         </div>
         <div className="py-16">
