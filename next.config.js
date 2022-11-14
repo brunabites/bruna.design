5b798e256f4511095508ffe6351cd54a0cb8d35a
Bruna Bites <bruna.bites@gmail.com>
2022-11-14 17:46:49 -0300

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -55,6 +55,11 @@ const securityHeaders = [
   },
 ];
 const nextConfig = {
+  experimental: {
+    images: {
+      layoutRaw: true,
+    },
+  },
   swcMinify: true,
   reactStrictMode: true,
   images: {
