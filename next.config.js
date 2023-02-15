32f5a9a721bfc4614bc12c26906075c331d1226f
Bruna Bites <bruna.bites@gmail.com>
2023-02-15 14:44:43 -0300

    Remove plausible

--- next.config.js
+++ next.config.js
@@ -77,18 +77,6 @@ const nextConfig = {
       },
     ];
   },
-  async rewrites() {
-    return [
-      {
-        source: '/js/script.js',
-        destination: 'https://plausible.io/js/script.js',
-      },
-      {
-        source: '/api/event', // Or '/api/event/' if you have `trailingSlash: true` in this config
-        destination: 'https://plausible.io/api/event',
-      },
-    ];
-  },
   webpack: (config, { dev, isServer }) => {
     // Replace React with Preact only in client production build
     if (!dev && !isServer) {
