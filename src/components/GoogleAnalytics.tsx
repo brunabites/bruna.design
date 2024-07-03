5c0e9fc21be4d2eb7cbf1e46884c0161200c116d
Bruna Bites <bruna.bites@gmail.com>
2024-07-03 18:34:30 -0300

    Update GoogleAnalytics.tsx

--- src/components/GoogleAnalytics.tsx
+++ src/components/GoogleAnalytics.tsx
@@ -5,20 +5,21 @@ const GoogleAnalytics = () => {
   return (
     <>
       <Script
+        async
         strategy='lazyOnload'
         src={`https://www.googletagmanager.com/gtag/js?id=G-B7B4SVX90B`}
       />
 
       <Script id='' strategy='lazyOnload'>
         {`
-              window.dataLayer = window.dataLayer || [];
-              function gtag(){dataLayer.push(arguments);}
-              gtag('js', new Date());
-              gtag('config', 'G-B7B4SVX90B', {
-              page_path: window.location.pathname,
-              });
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+          
+            gtag('config', 'G-B7B4SVX90B');
           `}
       </Script>
+
     </>
   );
 };
